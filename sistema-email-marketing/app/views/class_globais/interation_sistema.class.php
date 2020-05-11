a0f8a8e65b458445031d6a5e9bb086bff72d6528
Jonas gomes <dezbites@gmail.com>
2020-05-10 21:55:33 -0300

    adicionado time para execulção da janela

--- sistema-email-marketing/app/views/class_globais/interation_sistema.class.php
+++ sistema-email-marketing/app/views/class_globais/interation_sistema.class.php
@@ -1,7 +1,6 @@
 <?php
 
 class interation_sistema{
-
     public static function interation_js(array $jsgeral=[]){
         if( is_array($jsgeral) ){
             for( $i=0; $i < count($jsgeral); $i++){
--- sistema-email-marketing/app/views/class_globais/js/dbites-window.js
+++ sistema-email-marketing/app/views/class_globais/js/dbites-window.js
@@ -14,21 +14,23 @@ function wind(valo,vls){
         const janela   = document.querySelector(".dbites-window");
         const fundp    = document.createElement("div");
         const tituloJ  = janela.getAttribute("dbites-title");
-        fundp.id="";
-        fundp.className="";
+        fundp.id       = "";
+        fundp.className= "";
         fundp.innerHTML=`<div class='titulo' style='margin-top:-39px;color:#fff;
         font-weight:bold;text-align:center;'>${tituloJ}<span id='fechar' style='position:absolute;right:20%;'><img width='20' src='app/views/imgs/fechar.png' /></span></div>`;
         janela.setAttribute("style","display:grid;justify-content:center;align-items:center;position:fixed;z-index:999;width:100vw;height:100vh;top:0;background-color:rgba(0,0,0,0.8);");
-        fundp.setAttribute("style",`padding:10px;border-radius:7px 7px 0px 0px;border-top:40px solid #5cc1bd;width:60vw;height:50vh;background-color:#fff;box-shadow:0px 0px 30px
+        fundp.setAttribute("style",`margin-top:-150px;opacity:0;padding:10px;border-radius:7px 7px 0px 0px;border-top:40px solid #5cc1bd;width:60vw;height:50vh;background-color:#fff;box-shadow:0px 0px 30px
         rgba(0,0,0,0.3);transition:all 0.3s;`);
         fundp.innerHTML += "<br>"+valo;
         janela.appendChild(fundp);
+        setTimeout(function(){
+            fundp.style.marginTop="0px";
+            fundp.style.opacity="1";
+        },350);
         document.querySelector("#fechar").onclick=function(){
             janela.style.display="none";
             fundp.remove();
             desfoq(false);
         }
-    }else{
-
     }
 }
